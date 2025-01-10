52c55de713c47870cf23b25bb53dc4aa98260bb7
Messias-Bit <slaime227@gmail.com>
2025-01-10 17:40:44 -0300

    Update generateScript.js

--- functions/generateScript.js
+++ functions/generateScript.js
@@ -104,57 +104,53 @@ scriptCode += `    GuiControl,, StatusText, % message\n`;
 scriptCode += `    SetTimer, ClearStatus, -4000\n`;
 scriptCode += `}\n\n`;
 
+scriptCode += `Gui, 1:New\n`;
+scriptCode += `Gui, 1:+AlwaysOnTop\n`;
+scriptCode += `Gui, 1:Color, 1E293B, 243449\n`;
+scriptCode += `Gui, 1:Margin, 20, 20\n\n`;
+scriptCode += `Gui, 1:Font, s12 bold cE2E8F0\n`;
+scriptCode += `Gui, 1:Add, Text, x20 y20 w340 h30 Center, AutoScript RCC - Controle\n\n`;
+scriptCode += `Gui, 1:Font, s10 normal cE2E8F0\n`;
+scriptCode += `Gui, 1:Add, GroupBox, x20 y60 w340 h180, Controles\n\n`;
+scriptCode += `Gui, 1:Add, Text, x40 y90, Velocidade do Script:\n`;
+scriptCode += `Gui, 1:Add, Slider, x40 y110 w300 vSpeedSlider gUpdateSpeed Range6-8, 6\n`;
+scriptCode += `Gui, 1:Add, Text, x40 y140 w300 vSpeedText, % "Intervalo: 6.0 segundos"\n\n`;
+scriptCode += `Gui, 1:Add, Button, x40 y170 w90 h30 gStartScript vStartButton, Iniciar\n`;
+scriptCode += `Gui, 1:Add, Button, x145 y170 w90 h30 gPauseScript vPauseButton Disabled, Pausar\n`;
+scriptCode += `Gui, 1:Add, Button, x250 y170 w90 h30 gReloadScript, Recarregar\n\n`;
+scriptCode += `Gui, 1:Font, s9 bold\n`;
+scriptCode += `Gui, 1:Add, Text, x20 y250 w340 h30 vStatusText cFF4444 Center\n\n`;
+scriptCode += `Gui, 1:Font, s8\n`;
+scriptCode += `Gui, 1:Add, Text, x20 y290 w340 Center c94A3B8, Desenvolvido por cralw16\n\n`;
+
+scriptCode += `Gui, 3:+AlwaysOnTop +ToolWindow -SysMenu\n`;
+scriptCode += `Gui, 3:Color, 1E293B, 243449\n`;
+scriptCode += `Gui, 3:Margin, 20, 20\n`;
+scriptCode += `Gui, 3:Font, s10 bold cE2E8F0\n`;
+scriptCode += `Gui, 3:Add, Text, x20 y20 w200 h30, O aluno respondeu a pergunta?\n`;
+scriptCode += `Gui, 3:Font, s10 normal\n`;
+scriptCode += `Gui, 3:Add, Button, x20 y60 w80 h30 gAnswerYes, Sim\n`;
+scriptCode += `Gui, 3:Add, Button, x110 y60 w80 h30 gAnswerNo, Não\n\n`;
+
 scriptCode += `if (username = "" or username = "ERROR") {\n`;
-scriptCode += `    Gosub, ShowConfigGUI\n`;
+scriptCode += `    Gosub, ShowConfig\n`;
 scriptCode += `} else {\n`;
-scriptCode += `    Gosub, ShowMainGUI\n`;
+scriptCode += `    Gui, 1:Show, w380 h320, AutoScript RCC\n`;
 scriptCode += `}\n\n`;
 
-scriptCode += `return\n\n`;
-
-scriptCode += `ShowMainGUI:\n`;
-scriptCode += `Gui, New\n`;
-scriptCode += `Gui, +AlwaysOnTop\n`;
-scriptCode += `Gui, Color, 1E293B, 243449\n`;
-scriptCode += `Gui, Margin, 20, 20\n\n`;
-scriptCode += `Gui, Font, s12 bold cE2E8F0\n`;
-scriptCode += `Gui, Add, Text, x20 y20 w340 h30 Center, AutoScript RCC - Controle\n\n`;
-
-scriptCode += `Gui, Font, s10 normal cE2E8F0\n`;
-scriptCode += `Gui, Add, GroupBox, x20 y60 w340 h180, Controles\n\n`;
-
-scriptCode += `Gui, Add, Text, x40 y90, Velocidade do Script:\n`;
-scriptCode += `Gui, Add, Slider, x40 y110 w300 vSpeedSlider gUpdateSpeed Range6-8, 6\n`;
-scriptCode += `Gui, Add, Text, x40 y140 w300 vSpeedText, % "Intervalo: 6.0 segundos"\n\n`;
-
-scriptCode += `Gui, Add, Button, x40 y170 w90 h30 gStartScript vStartButton, Iniciar\n`;
-scriptCode += `Gui, Add, Button, x145 y170 w90 h30 gPauseScript vPauseButton Disabled, Pausar\n`;
-scriptCode += `Gui, Add, Button, x250 y170 w90 h30 gReloadScript, Recarregar\n\n`;
-
-scriptCode += `Gui, Font, s9 bold\n`;
-scriptCode += `Gui, Add, Text, x20 y250 w340 h30 vStatusText cFF4444 Center\n\n`;
-
-scriptCode += `Gui, Font, s8\n`;
-scriptCode += `Gui, Add, Text, x20 y290 w340 Center c94A3B8, Desenvolvido por cralw16\n\n`;
-
-scriptCode += `Gui, Show, w380 h320, AutoScript RCC\n`;
-scriptCode += `return\n\n`;
-
-scriptCode += `ShowConfigGUI:\n`;
+scriptCode += `ShowConfig:\n`;
 scriptCode += `Gui, 2:New\n`;
 scriptCode += `Gui, 2:+AlwaysOnTop\n`;
 scriptCode += `Gui, 2:Color, 1E293B, 243449\n`;
-scriptCode += `Gui, 2:Margin, 20, 20\n\n`;
+scriptCode += `Gui, 2:Margin, 20, 20\n`;
 scriptCode += `Gui, 2:Font, s12 bold cE2E8F0\n`;
-scriptCode += `Gui, 2:Add, Text, x20 y20 w300 Center, Configuração Inicial\n\n`;
+scriptCode += `Gui, 2:Add, Text, x20 y20 w300 Center, Configuração Inicial\n`;
 scriptCode += `Gui, 2:Font, s10 normal cE2E8F0\n`;
 scriptCode += `Gui, 2:Add, Text, x20 y60 w300, Digite seu nickname:\n`;
-scriptCode += `Gui, 2:Add, Edit, x20 y90 w300 h30 vUserNickname\n\n`;
-scriptCode += `Gui, 2:Add, Button, x95 y140 w150 h30 gSaveUsername, Confirmar\n\n`;
+scriptCode += `Gui, 2:Add, Edit, x20 y90 w300 h30 vUserNickname\n`;
+scriptCode += `Gui, 2:Add, Button, x95 y140 w150 h30 gSaveUsername, Confirmar\n`;
 scriptCode += `Gui, 2:Show, w340 h190, Configuração\n`;
 scriptCode += `return\n\n`;
-
-scriptCode += `SetTimer, CheckConfigFile, 1000\n\n`;
 
 scriptCode += `SaveUsername:\n`;
 scriptCode += `Gui, 2:Submit, NoHide\n`;
@@ -165,28 +161,34 @@ scriptCode += `}\n`;
 scriptCode += `username := UserNickname\n`;
 scriptCode += `IniWrite, %username%, config.ini, Configuracao, Username\n`;
 scriptCode += `Gui, 2:Destroy\n`;
-scriptCode += `Gosub, ShowWarningGUI\n`; 
+scriptCode += `Gosub, ShowWarning\n`;
 scriptCode += `return\n\n`;
 
-scriptCode += `ShowWarningGUI:\n`; 
+scriptCode += `ShowWarning:\n`;
 scriptCode += `Gui, 4:New, +AlwaysOnTop\n`;
 scriptCode += `Gui, 4:Color, 1E293B, 243449\n`;
-scriptCode += `Gui, 4:Margin, 20, 20\n\n`;
+scriptCode += `Gui, 4:Margin, 20, 20\n`;
 scriptCode += `Gui, 4:Font, s12 bold cE2E8F0\n`;
-scriptCode += `Gui, 4:Add, Text, x20 y20 w360 Center, Aviso Importante\n\n`;
+scriptCode += `Gui, 4:Add, Text, x20 y20 w360 Center, Aviso Importante\n`;
 scriptCode += `Gui, 4:Font, s10 normal cE2E8F0\n`;
-scriptCode += `Gui, 4:Add, Text, x20 y60 w360, Foi criado um arquivo config.ini no mesmo local deste script. Por favor, não exclua este arquivo, pois ele guarda suas configurações de nickname.\n\n`;
-scriptCode += `Gui, 4:Add, Button, x145 y140 w110 h30 gCloseWarning, Entendi\n\n`;
+scriptCode += `Gui, 4:Add, Text, x20 y60 w360, Foi criado um arquivo config.ini no mesmo local deste script. Por favor, não exclua este arquivo, pois ele guarda suas configurações de nickname.\n`;
+scriptCode += `Gui, 4:Add, Button, x145 y140 w110 h30 gCloseWarning, Entendi\n`;
 scriptCode += `Gui, 4:Show, w400 h190, Aviso\n`;
 scriptCode += `return\n\n`;
 
 scriptCode += `CloseWarning:\n`;
 scriptCode += `Gui, 4:Destroy\n`;
-scriptCode += `Gosub, ShowMainGUI\n`; 
+scriptCode += `Gui, 1:Show, w380 h320, AutoScript RCC\n`;
+scriptCode += `SetTimer, CheckConfigFile, 1000\n`;
 scriptCode += `return\n\n`;
 
-scriptCode += `Gui, Show, w380 h320, AutoScript RCC\n`;
-scriptCode += `SetTimer, CheckConfigFile, 1000\n`;
+scriptCode += `CheckConfigFile:\n`;
+scriptCode += `if (!FileExist("config.ini")) {\n`;
+scriptCode += `    SetTimer, CheckConfigFile, Off\n`;
+scriptCode += `    Gui, 1:Hide\n`;
+scriptCode += `    username := ""\n`;
+scriptCode += `    Gosub, ShowConfig\n`;
+scriptCode += `}\n`;
 scriptCode += `return\n\n`;
 
 scriptCode += `UpdateSpeed:\n`;
